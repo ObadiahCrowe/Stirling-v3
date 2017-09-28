e6b0c25dddff345844eb2f2045f0f19e0bfe34bc
St1rling <obadiahpcrowe@gmail.com>
2017-09-28 19:39:37 +0930

    Announcement API finalised

--- src/main/java/com/obadiahpcrowe/stirling/api/AnnouncementAPI.java
+++ src/main/java/com/obadiahpcrowe/stirling/api/AnnouncementAPI.java
@@ -13,6 +13,8 @@ import com.obadiahpcrowe.stirling.resources.AttachableResource;
 import com.obadiahpcrowe.stirling.util.UtilFile;
 import com.obadiahpcrowe.stirling.util.msg.MsgTemplate;
 import com.obadiahpcrowe.stirling.util.msg.StirlingMsg;
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,7 +23,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.rmi.CORBA.Util;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
@@ -76,18 +80,20 @@ public class AnnouncementAPI implements APIController {
 
         UUID uuid = UUID.randomUUID();
         File out = new File(UtilFile.getInstance().getStorageLoc() + File.separator + "Announcements" +
-          File.separator + uuid + File.separator + "banner.jpg");
+          File.separator + uuid);
+        File banner = new File(out + File.separator + "banner.jpg");
         try {
             if (!out.exists()) {
                 out.mkdir();
             }
 
-            file.transferTo(out);
+
+            file.transferTo(banner);
         } catch (IOException e) {
             return gson.toJson(new StirlingMsg(MsgTemplate.UNEXPECTED_ERROR, account.getLocale(), "creating the announcement"));
         }
 
-        return manager.postAnnouncement(account, uuid, announcementType, new AttachableResource(uuid, out.getPath()),
+        return manager.postAnnouncement(account, uuid, announcementType, new AttachableResource(uuid, banner.getPath()),
           title, desc, content, resourceNames, targetAudiences, tags);
     }
 
@@ -130,6 +136,32 @@ public class AnnouncementAPI implements APIController {
           announcement.getTargetAudience().get(0).getFriendlyName()));
     }
 
+    @CallableAPI(fields = { "accountName", "password", "uuid" })
+    @RequestMapping(value = "/stirling/v3/announcements/get/banner", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
+    public byte[] getAnnouncementBanner(@RequestParam("accountName") String accountName,
+                                        @RequestParam("password") String password,
+                                        @RequestParam("uuid") String rawUuid) {
+        StirlingAccount account = accountManager.getAccount(accountName);
+        if (account == null) {
+            return null;
+        }
+
+        if (!accountManager.validCredentials(accountName, password)) {
+            return null;
+        }
+
+        try {
+            StirlingAnnouncement announcement = manager.getAnnouncement(UUID.fromString(rawUuid));
+            if (announcement.getTargetAudience().contains(account.getAccountType())) {
+                InputStream inputStream = new FileInputStream(manager.getAnnouncement(UUID.fromString(rawUuid)).getBannerImage().getFile());
+                return IOUtils.toByteArray(inputStream);
+            }
+            return null;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
     @CallableAPI(fields = { "accountName", "password" })
     @RequestMapping(value = "/stirling/v3/announcements/getAll", method = RequestMethod.GET)
     public String getAnnouncements(@RequestParam("accountName") String accountName,
@@ -146,13 +178,13 @@ public class AnnouncementAPI implements APIController {
         return gson.toJson(manager.getAnnouncements(account));
     }
 
-    @CallableAPI(fields = { "accountName", "password", "field", "value" })
-    @RequestMapping(value = "/stirling/v3/announcements/edit", method = RequestMethod.GET)
-    public String editAnnouncement(@RequestParam("accountName") String accountName,
-                                   @RequestParam("password") String password,
-                                   @RequestParam("uuid") String rawUuid,
-                                   @RequestParam("field") String field,
-                                   @RequestParam("value") String value) {
+    @CallableAPI(fields = { "accountName", "password", "uuid", "field", "value" })
+    @RequestMapping(value = "/stirling/v3/announcements/update/field", method = RequestMethod.GET)
+    public String updateAnnouncementField(@RequestParam("accountName") String accountName,
+                                          @RequestParam("password") String password,
+                                          @RequestParam("uuid") String rawUuid,
+                                          @RequestParam("field") String field,
+                                          @RequestParam("value") String value) {
         StirlingAccount account = accountManager.getAccount(accountName);
         if (account == null) {
             return gson.toJson(new StirlingMsg(MsgTemplate.ACCOUNT_DOES_NOT_EXIST, StirlingLocale.ENGLISH, accountName));
@@ -185,5 +217,37 @@ public class AnnouncementAPI implements APIController {
         }
 
         return manager.updateField(UUID.fromString(rawUuid), field, value);
+    }
+
+    @CallableAPI(fields = { "accountName", "password", "banner" })
+    @RequestMapping(value = "/stirling/v3/announcements/update/banner", method = RequestMethod.GET)
+    public String updateAnnouncementBanner(@RequestParam("accountName") String accountName,
+                                           @RequestParam("password") String password,
+                                           @RequestParam("uuid") String rawUuid,
+                                           @RequestParam("banner") MultipartFile file) {
+        StirlingAccount account = accountManager.getAccount(accountName);
+        if (account == null) {
+            return gson.toJson(new StirlingMsg(MsgTemplate.ACCOUNT_DOES_NOT_EXIST, StirlingLocale.ENGLISH, accountName));
+        }
+
+        if (!accountManager.validCredentials(accountName, password)) {
+            return gson.toJson(new StirlingMsg(MsgTemplate.PASSWORD_INCORRECT, StirlingLocale.ENGLISH, accountName));
+        }
+
+        try {
+            File out = new File(UtilFile.getInstance().getStorageLoc() + File.separator + "Announcements" +
+              File.separator + rawUuid);
+
+            if (!out.exists()) {
+                out.mkdir();
+            }
+
+            File banner = new File(out + File.separator + "banner.jpg");
+
+            file.transferTo(banner);
+            return gson.toJson(new StirlingMsg(MsgTemplate.ANNOUNCEMENT_EDITED, StirlingLocale.ENGLISH, rawUuid));
+        } catch (IOException e) {
+            return gson.toJson(new StirlingMsg(MsgTemplate.UNEXPECTED_ERROR, account.getLocale(), "updating the banner image"));
+        }
     }
 }
